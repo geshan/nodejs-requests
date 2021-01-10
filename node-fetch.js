d00816cfe2de938150b2211d7e772b7ab1fc55bd
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-01-10 22:21:28 +1100

    Add node-fetch with get users example async-await

---
+++ node-fetch.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+(async () => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const headerDate = res.headers && res.headers.get('date') ? res.headers.get('date') : 'no response date';
+    console.log('Status Code:', res.status);
+    console.log('Date in Response header:', headerDate);
+
+    const users = await res.json();
+    for(user of users) {
+      console.log(`Got user with id: ${user.id}, name: ${user.name}`);
+    }
+  } catch (err) {
+    console.log(err.message); //can be console.error
+  }
+})();
