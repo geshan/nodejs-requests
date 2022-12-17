49195d295db43ee1df9681395f5793e5ac989ea9
B. Burt <97593291+beeburrt@users.noreply.github.com>
2022-12-16 22:12:42 -0800

    Update axios.js

--- axios.js
+++ axios.js
@@ -1,6 +1,11 @@
 const axios = require('axios');
 
-axios.get('https://jsonplaceholder.typicode.com/users')
+axios
+  .get('https://jsonplaceholder.typicode.com/users', {
+    headers: {
+      "Accept-Encoding": "gzip,deflate,compress",
+    },
+  })
   .then(res => {
     const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
     console.log('Status Code:', res.status);
